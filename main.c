c44b7ce2247f3328fcda9efc317e50e905261bdb
Tony Rizzo <trizzo@koteray.com>
2018-03-23 13:18:42 -0400

    Initial commit

---
+++ main.c
@@ -0,0 +1,20 @@
+//
+//  main.c
+//  AGoodStart
+//
+//  Created by Tony Rizzo on 11/7/17.
+//  Copyright © 2017 Koteray. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+
+    // Print the beginning of th enovel
+    printf("It was the best of times.\n");
+    printf("It was the worst of times.\n");
+    /* Is that actually any good?
+       Maybe it needs a rewrite. */
+    
+    return 0;
+}
